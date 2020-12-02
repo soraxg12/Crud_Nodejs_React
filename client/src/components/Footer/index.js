76eb0ecabecec31d45ad0b073da7432cfaf3c74d
soraxg12 <contadevervidio@gmail.com>
2020-12-02 00:30:23 -0300

    adding component Footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import "./styles.css"
+
+function Footer(){
+    return(
+        <div className='footer'>
+            <p>final work &#0169;</p>
+        </div>
+    )
+}
+
+export default Footer;
